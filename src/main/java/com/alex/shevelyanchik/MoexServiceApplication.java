58a2f2a4543f552112bb8b2c51f02bfe5aee997c
Aliaksandr Shavialianchyk <aliaksandr_shavialianchyk@epam.com>
2022-12-27 00:55:05 +0300

    init commit

---
+++ src/main/java/com/alex/shevelyanchik/MoexServiceApplication.java
@@ -0,0 +1,12 @@
+package com.alex.shevelyanchik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoexServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoexServiceApplication.class, args);
+    }
+}
